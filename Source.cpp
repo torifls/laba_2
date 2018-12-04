0442f4946b77107b00d7ab299f02cf99b478dcd7
torifls <torifls@bk.ru>
2018-12-04 11:35:22 +0300

    изменение

--- Source.cpp
+++ Source.cpp
@@ -4,9 +4,9 @@
 
 using namespace std;
 
-
-//С использованием файловых и строковых потоков написать программу, которая считывает из текстового файла три предложения и выводит их в обратном порядке.
-
+/*
+С использованием файловых и строковых потоков написать программу, которая считывает из текстового файла три предложения и выводит их в обратном порядке.
+*/
 
 int main() {
 	string path;
@@ -14,51 +14,52 @@ int main() {
 	getline(cin, path);
 
 	fstream f;
+	f.exceptions(ifstream::failbit);
+	try {
+		f.open(path.c_str(), ios::in);
+		stringstream ss1;
+		stringstream ss2;
+		stringstream ss3;
+		char c = ' ';
 
-	f.open(path.c_str(), ios::in);
+		while (!f.eof() && (c != '.' && c != '!' && c != '?' && c != '\n')) {
+			f.get(c);
 
-	if (!f) {
-		cout << "Error: can't open file!" << endl;
-		return 0;
-	}
+			ss1 << c;
+		}
 
-	stringstream ss1;
-	stringstream ss2;
-	stringstream ss3;
-	char c=' ';
-
-	while (!f.eof() && (c != '.' && c != '!' && c != '?' && c != '\n')) {
 		f.get(c);
 
-		ss1 << c;
-	}
+		while (!f.eof() && (c != '.' && c != '!' && c != '?' && c != '\n')) {
+			ss2 << c;
+			f.get(c);
+		}
 
-	f.get(c);
-
-	while (!f.eof() && (c != '.' && c != '!' && c != '?' && c != '\n')) {
 		ss2 << c;
 		f.get(c);
-	}
 
-	ss2 << c;
-	f.get(c);
+		while (!f.eof() && (c != '.' && c != '!' && c != '?' && c != '\n')) {
+			ss3 << c;
+			f.get(c);
+		}
 
-	while (!f.eof() && (c != '.' && c != '!' && c != '?' && c != '\n')) {
 		ss3 << c;
-		f.get(c);
+		f.close();
+
+		string s;
+		cout << "Reverse: ";
+
+		getline(ss3, s);
+		cout << s << " ";
+		getline(ss2, s);
+		cout << s << " ";
+		getline(ss1, s);
+		cout << s << " ";
+		//system("pause");
 	}
-
-	ss3 << c;
-	f.close();
-
-	string s;
-	cout << "Reverse: ";
-
-	getline(ss3, s);
-	cout << s << " ";
-	getline(ss2, s);
-	cout << s << " ";
-	getline(ss1, s);
-	cout << s << " ";
+	catch (const ifstream::failure& exc)
+	{
+			cout << "Error: can't open file!" << endl<<exc.what()<<endl;
+	}
 	return 0;
 }
